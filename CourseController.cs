073b2ac18e44aad1231c371fd7d64d93f5d0c515
genlarkins <54943134+genlarkins@users.noreply.github.com>
2020-07-21 21:37:58 -0400

    Add files via upload

---
+++ CourseController.cs
@@ -0,0 +1,33 @@
+﻿using ShoppingSPA.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace ShoppingSPA.Controllers.Api
+{
+    [RoutePrefix("api/Course")]
+    public class CourseController : ApiController
+    {
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok(DbMock.Classes);
+        }
+
+        [HttpPost]
+        [Route("AddOrUpdate")]
+        public async Task<IHttpActionResult> AddOrUpdate([FromBody] Course course)
+        {
+            if (course != null)
+            {
+                DbMock.Classes.Add(course);
+            }
+
+            return Ok(course);
+        }
+    }
+}
