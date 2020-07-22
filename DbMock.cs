ccfb89036e80cd21b0822e86a137323f99fcfe41
genlarkins <54943134+genlarkins@users.noreply.github.com>
2020-07-21 21:39:09 -0400

    Add files via upload

---
+++ DbMock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingSPA.Persistence
+{
+    public static class DbMock
+    {
+        public static List<Semester> Semesters = new List<Semester> { 
+            new Semester{Id = 1, Name = "Fall2019"},
+            new Semester{Id = 2, Name = "Spring2020"},
+            new Semester{Id = 3, Name = "Summer2020"}
+        };
+
+        public static List<Course> Classes = new List<Course> {
+            new Course{SemesterId = 1, Id = 0, CourseName = "Software Engingeering 1", CourseCode = "CEN 4020", Enrollment = 120, Instructor = "Sonya P"},
+            new Course{SemesterId = 2, Id = 1, CourseName = "C# Programming", CourseCode = "CIS 4930", Instructor = "Chris Mills", Enrollment = 43},
+            new Course{SemesterId =3, Id = 2, CourseName = "Intro to Programming", CourseCode = "COP 3330", Instructor = "Chris Mills", Enrollment = 250},
+            new Course{SemesterId = 2, Id = 3, CourseName = "Op Sys", CourseCode = "COP 4610", Instructor = "An-i Wang", Enrollment = 200}
+        };
+    }
+
+    public partial class Semester
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public partial class Course
+    {
+        public int SemesterId { get; set; }
+
+        public int Id { get; set; }
+
+        public string CourseName { get; set; }
+
+        public string CourseCode { get; set; }
+
+        public string Instructor { get; set; }
+
+        public int Enrollment { get; set; }
+
+
+    }
+}
---
+++ app.js
@@ -0,0 +1,129 @@
+﻿var app = angular.module("ABET", ["ngRoute"]);
+app.controller('mainCtrl', function ($scope, $http) {
+    $scope.nextSemester = 4;
+    //$scope.semesters = [{ id: 1, name: 'Fall2019' }, { id: 2, name: 'Spring2020' }, { id: 3, name: 'Summer2020' }];
+    $scope.nextClass = 4;
+    $http({
+        method: 'GET',
+        url: 'api/Semester/Get'
+    }).then(function success(response) {
+        $scope.semesters = response.data;
+    }, function failure() {
+
+    });
+
+    $http({
+        method: 'GET',
+        url:'api/Semester/GetById/2'
+    }).then(function success(response) {
+        $scope.selectedSemester = response.data;
+    }, function failure() {
+
+    });
+
+    
+
+    $scope.selectSemester = function (semester) {
+        $scope.addingSemester = false;
+        $scope.selectedClass = undefined;
+        $scope.selectedSemester = semester;
+    }
+
+   // $scope.classes = [{ id: 0, course: { id: 0, courseName: 'Software Engineering 1', courseCode: 'CEN 4020' }, semesterId: 2, instructor: 'Chris Mills', syllabus: null, canvasLink: '', enrollment: 120 },
+    //    { id: 1, course: { id: 1, courseName: 'C# Application Development', courseCode: 'CIS 4930' }, semesterId: 1, instructor: 'Chris Mills', syllabus: null, canvasLink: '', enrollment: 17 }];
+
+    $scope.selectClass = function (cl) {
+        $scope.addingClass = false;
+        $scope.selectedClass = cl;
+    }
+
+    $scope.showNewClass = function () {
+        $scope.addingClass = true;
+        $scope.selectedClass = new Object();
+        $scope.selectedClass.course = new Object();
+    }
+
+    $scope.addClass = function () {
+        $scope.selectedClass.semesterId = $scope.selectedSemester.Id;
+        // $scope.classes.push($scope.selectedClass);
+        $scope.selectedClass.id = $scope.nextClass;
+        $scope.nextClass += 1;
+
+        $http({
+            method: 'POST',
+            url: 'api/Course/AddOrUpdate',
+            data: $scope.selectedClass
+        }).then(function success(response) {
+            $scope.classes.push(response.data);
+        }, function failure() {
+
+        });
+
+        $scope.selectedClass = undefined;
+        $scope.addingClass = false;
+    }
+
+    $scope.cancelAddClass = function () {
+        $scope.selectedClass = undefined;
+        $scope.addingClass = false;
+    }
+
+    $scope.showNewSemester = function () {
+        $scope.selectedSemester = undefined;
+        $scope.addingSemester = true;
+    }
+    $scope.addSemester = function () {
+        $scope.selectedSemester.id = $scope.nextSemester;
+        $scope.nextSemester += 1;
+        
+
+        $http({
+            method: 'POST',
+            url: 'api/Semester/AddOrUpdate',
+            data: $scope.selectedSemester
+        }).then(function success(response) {
+            $scope.semesters.push(response.data);
+        }, function failure() {
+
+        });
+
+        $scope.selectedSemester = undefined;
+        $scope.addingSemester = false;
+    }
+
+    $scope.cancelAddSemester = function () {
+        $scope.selectedSemester = undefined;
+        $scope.addingSemester = false;
+    }
+
+    $scope.removeSemester = function (id) {
+        var indexToDelete = -1;
+        for (i = 0; i < $scope.semesters.length; i++) {
+            if ($scope.semesters[i].id === id) {
+                indexToDelete = i;
+                break;
+            }
+        }
+        if (indexToDelete >= 0) {
+            $scope.semesters.splice(indexToDelete, 1);
+        }
+
+    }
+
+    $scope.removeClass = function (id) {
+        var indexToDelete = -1;
+        for (i = 0; i < $scope.classes.length; i++) {
+            if ($scope.classes[i].id === id) {
+                indexToDelete = i;
+                break;
+            }
+        }
+        if (indexToDelete >= 0) {
+            $scope.classes.splice(indexToDelete, 1);
+        }
+
+        if ($scope.selectedClass.id == id) {
+            $scope.selectedClass = undefined;
+        }
+    }
+});
